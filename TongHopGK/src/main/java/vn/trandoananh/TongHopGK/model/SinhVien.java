e07832fcc15c9f06fdc5b177c4549cea4d00618d
Doananh2004 <trandoananhnt@gmail.com>
2025-03-25 10:23:36 +0700

    Tạo lớp SinhVien

---
+++ TongHopGK/src/main/java/vn/trandoananh/TongHopGK/model/SinhVien.java
@@ -0,0 +1,14 @@
+package vn.trandoananh.TongHopGK.model;
+
+public class SinhVien {
+	
+	public String MSSV;
+	public String HoTen;
+	public double DiemTB;
+	
+	public SinhVien(String mssv, String hoTen, double diemTB) {
+		MSSV = mssv;
+		HoTen = hoTen;
+		DiemTB = diemTB;
+	}
+}
