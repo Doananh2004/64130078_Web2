90642229e49b3fe5121037750faa3aa45dbe64df
Doananh2004 <trandoananhnt@gmail.com>
2025-03-30 06:18:18 +0700

    Ôn tập giữa kỳ

--- TongHopGK/src/main/java/vn/trandoananh/TongHopGK/controllers/HomeController.java
+++ TongHopGK/src/main/java/vn/trandoananh/TongHopGK/controllers/HomeController.java
@@ -1,28 +1,53 @@
 package vn.trandoananh.TongHopGK.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import vn.trandoananh.TongHopGK.model.SinhVien;
 
 @Controller
 public class HomeController {
+	
+	ArrayList<SinhVien> sinhVienList = new ArrayList<SinhVien>(Arrays.asList(
+			new SinhVien("64130078", "Trần Doãn Anh", 7.5),
+			new SinhVien("6413005", "Hoàng Duy An", 9.0)
+			));
 
 	@GetMapping("/")
 	public String HomePage() {
-		return "frontEndViews/index";
+		return "index";
 	}
 	
 	@GetMapping("/about")
 	public String AboutPage() {
-		return "frontEndViews/about";
+		return "about";
 	}
 	
 	@GetMapping("/studentList")
-	public String StudentListPage() {
-		return "frontEndViews/studentList";
+	public String StudentListPage(ModelMap m) {
+		m.addAttribute("svs", sinhVienList);
+		return "studentList";
 	}
 	
 	@GetMapping("/addNew")
 	public String AddNew() {
-		return "frontEndViews/addNew";
+		return "newStudent";
+	}
+	
+	@PostMapping("/add")
+	public String login(ModelMap model, HttpServletRequest request) {
+		String mssv = request.getParameter("mssv");
+		String hoTen = request.getParameter("hoTen");
+		double diemTB = Double.parseDouble(request.getParameter("diemTB"));
+		SinhVien student = new SinhVien(mssv, hoTen, diemTB);
+		sinhVienList.add(student);
+		model.addAttribute("sv", sinhVienList);
+		return "list";
 	}
 }
--- TongHopGK/src/main/java/vn/trandoananh/TongHopGK/model/SinhVien.java
+++ TongHopGK/src/main/java/vn/trandoananh/TongHopGK/model/SinhVien.java
@@ -2,14 +2,38 @@ package vn.trandoananh.TongHopGK.model;
 
 public class SinhVien {
 	
-	public String MSSV;
-	public String HoTen;
-	public float DiemTB;
+	public String mssv;
+	public String hoTen;
+	public double diemTB;
 	
-	public SinhVien(String mssv, String hoTen, float diemTB) {
-		MSSV = mssv;
-		HoTen = hoTen;
-		DiemTB = diemTB;
+	public SinhVien(String mssv, String hoTen, double diemTB) {
+		this.mssv = mssv;
+		this.hoTen = hoTen;
+		this.diemTB = diemTB;
 	}
 	
+	// Getter và Setter
+    public String getMssv() {
+        return mssv;
+    }
+
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public double getDiemTB() {
+        return diemTB;
+    }
+
+    public void setDiemTB(double diemTB) {
+        this.diemTB = diemTB;
+    }
 }
