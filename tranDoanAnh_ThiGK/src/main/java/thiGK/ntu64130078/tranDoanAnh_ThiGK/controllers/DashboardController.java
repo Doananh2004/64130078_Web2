66aa292d6630c13ee716e5a8ef4b7173abd234cb
Doananh2004 <trandoananhnt@gmail.com>
2025-04-01 09:21:21 +0700

    Tao danh sach page

--- tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/controllers/DashboardController.java
+++ tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/controllers/DashboardController.java
@@ -1,5 +1,27 @@
 package thiGK.ntu64130078.tranDoanAnh_ThiGK.controllers;
 
-public class DashboardController {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import thiGK.ntu64130078.tranDoanAnh_ThiGK.model.Page;
+import thiGK.ntu64130078.tranDoanAnh_ThiGK.model.Post;
+
+@Controller
+public class DashboardController {
+	
+	ArrayList<Page> pageList = new ArrayList<Page>(Arrays.asList(
+			new Page("1","Trang 1","p1","","")
+			));
+	
+	ArrayList<Post> postList = new ArrayList<Post>(Arrays.asList(
+			
+			));
+	
+	@GetMapping("/")
+	public String DashboardPage() {
+		return "dashboard";
+	}
 }
--- tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/model/Post.java
+++ tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/model/Post.java
@@ -2,4 +2,15 @@ package thiGK.ntu64130078.tranDoanAnh_ThiGK.model;
 
 public class Post {
 
+	public String id;
+	public String title;
+	public String content;
+	public String categoryId;
+	
+	public Post(String id, String title, String content, String categoryId) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.categoryId = categoryId;
+	}
 }
