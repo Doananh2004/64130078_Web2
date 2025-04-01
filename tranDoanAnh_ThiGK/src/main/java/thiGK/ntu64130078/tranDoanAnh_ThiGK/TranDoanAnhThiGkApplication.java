7fdbf49caa83bc9e935de7af110f2ac884e9c290
Doananh2004 <trandoananhnt@gmail.com>
2025-04-01 09:02:19 +0700

    Tạo dự án

---
+++ tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/TranDoanAnhThiGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130078.tranDoanAnh_ThiGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranDoanAnhThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranDoanAnhThiGkApplication.class, args);
+	}
+
+}
---
+++ tranDoanAnh_ThiGK/src/test/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/TranDoanAnhThiGkApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130078.tranDoanAnh_ThiGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranDoanAnhThiGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
