3ff6670454057c25e8306c099b1b86f6ccf60a86
Doananh2004 <trandoananhnt@gmail.com>
2025-04-01 10:48:03 +0700

    push lan cuoi

--- tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/controllers/DashboardController.java
+++ tranDoanAnh_ThiGK/src/main/java/thiGK/ntu64130078/tranDoanAnh_ThiGK/controllers/DashboardController.java
@@ -1,6 +1,7 @@
 package thiGK.ntu64130078.tranDoanAnh_ThiGK.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +10,12 @@ public class DashboardController {
 	
 	@GetMapping("/dashboard")
     public String Dashboard() {
+        return "dashboard";
+    }
+	
+	@GetMapping("/stats")
+    public String getDashboardStats(Model model) {
+        model.addAttribute("stats", "Dashboard statistics: 10 pages, 20 posts");
         return "dashboard";
     }
 }
