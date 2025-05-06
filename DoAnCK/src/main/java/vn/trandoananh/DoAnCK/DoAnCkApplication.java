84b3bfe12357bfd47e4772fed7228751cde16f60
Doananh2004 <trandoananhnt@gmail.com>
2025-05-06 16:37:31 +0700

    Đồ án CK

---
+++ DoAnCK/src/main/java/vn/trandoananh/DoAnCK/DoAnCkApplication.java
@@ -0,0 +1,13 @@
+package vn.trandoananh.DoAnCK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnCkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnCkApplication.class, args);
+	}
+
+}
---
+++ DoAnCK/src/test/java/vn/trandoananh/DoAnCK/DoAnCkApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.trandoananh.DoAnCK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnCkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
