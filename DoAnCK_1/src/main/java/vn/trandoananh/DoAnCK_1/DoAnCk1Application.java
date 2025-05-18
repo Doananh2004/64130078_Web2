b96dcf78dffd66d94608e60657e935843e89f223
Doananh2004 <trandoananhnt@gmail.com>
2025-05-18 10:56:05 +0700

    Dự án Web2

---
+++ DoAnCK_1/src/main/java/vn/trandoananh/DoAnCK_1/DoAnCk1Application.java
@@ -0,0 +1,13 @@
+package vn.trandoananh.DoAnCK_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnCk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnCk1Application.class, args);
+	}
+
+}
---
+++ DoAnCK_1/src/test/java/vn/trandoananh/DoAnCK_1/DoAnCk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.trandoananh.DoAnCK_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnCk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
